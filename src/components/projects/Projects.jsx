e58445ed1e8a161677aa7355a30769a155c0c59c
Carlos <carloscauantecnico@gmail.com>
2024-10-01 22:21:44 -0300

    Finish section projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,59 +1,94 @@
-import React from 'react';
-import Img1 from '../../assets/sp.png'
-import Img2 from '../../assets/salutis.png'
-import Img3 from '../../assets/usibras.png'
-import Img4 from '../../assets/Cyp.png'
-import './projects.css'
-
+import React, { useState } from 'react';
+import { FaHandPointUp } from 'react-icons/fa'; // Ícone do dedo apontando
+import Img1 from '../../assets/sp.png';
+import Img3 from '../../assets/apps3.png';
+import Img4 from '../../assets/apps2.png';
+import './projects.css';
 
 const Projects = () => {
+  const [activeCard, setActiveCard] = useState(null);
+
+  const handleCardClick = (index) => {
+    setActiveCard(activeCard === index ? null : index); // Alterna entre abrir e fechar
+  };
+
   return (
     <section id='projects' className='font-Poppins'>
-      <h5>Trabalhos/Participações</h5>
+      <h5>Trabalhos & Participações</h5>
       <h2 className='text-2xl'>Portfolio</h2>
 
-      <div className="container portfolio_container">
-        <article className="portifolio_item">
+      <div className="container portfolio_container font-Poppins">
+        <article 
+          className={`portifolio_item ${activeCard === 1 ? 'active' : ''}`} 
+          onClick={() => handleCardClick(1)}
+        >
           <div className="portfolio_item-image">
-            <img src={Img1} alt="" />
+            <img src={Img1} alt="SP T Leva" />
           </div>
           <h3>SP T LEVA</h3>
-            <div className="portfolio_description">
-              <p className='text-sm text-gray-400'>O App TLEVA, tem como objetivo levar a um novo nível de relacionamento com os Postos SP.</p>
-            </div>
-            <div className="portfolio_item-links">
-              <a href="https://play.google.com/store/apps/details?id=br.com.deway.spteleva&pcampaignid=web_share" className='btn btn-primary' target='_blank'>Ver</a>
-            </div>
+          <div className="portfolio_description">
+            <p>O SPTleva é um aplicativo que gerencia estoques e distribuição de combustíveis, automatizando processos e gerando relatórios para otimizar operações e garantir segurança. Desenvolvido com React Native, tive participação na criação de diversas telas e funcionalidades.</p>
+          </div>
+          <div className="portfolio_item-links">
+            <a 
+                href="https://play.google.com/store/apps/details?id=br.com.deway.spteleva&pcampaignid=web_share" 
+                className='btn' 
+                target='_blank'
+                onClick={(e) => e.stopPropagation()}
+              >
+                Ver
+            </a>
+          </div>
         </article>
-        <article className="portifolio_item">
+
+        <article 
+          className={`portifolio_item ${activeCard === 2 ? 'active' : ''}`} 
+          onClick={() => handleCardClick(2)}
+        >
           <div className="portfolio_item-image">
-            <img src={Img4} alt="" />
+            <img src={Img4} alt="CYP" />
           </div>
           <h3>CYP</h3>
-            <div className="portfolio_description">
-              <p className='text-sm text-gray-400'>O aplicativo para gamer, encontrar os seus players para montar o seu próprio squad, trio, duo, da forma que quiser, com quem quiser.</p>
-            </div>
-            <div className="portfolio_item-links">
-              {/* <a href="https://github.com/CarloszDev" className="btn" target='_blank'>Github</a> */}
-              <a href="https://play.google.com/store/apps/details?id=br.com.deway.cyapp&pcampaignid=web_share" className='btn btn-primary' target='_blank'>Ver</a>
-            </div>
-        </article>
-        <article className="portifolio_item">
-          <div className="portfolio_item-image">
-            <img src={Img3} alt="" />
+          <div className="portfolio_description">
+            <p>Desenvolvi e gerenciei um aplicativo voltado para gamers, que possibilita encontrar parceiros para formar squads, trios ou duos conforme preferirem. Gerenciei o projeto desde o zero, liderando uma equipe, criando funcionalidades cruciais, e oferecendo suporte no back-end.</p>
           </div>
-          <h3>USIBRAS</h3>
-            <div className="portfolio_description">
-              <p className='text-sm text-gray-400'>O aplicativo para gamer, encontrar os seus players para montar o seu próprio squad, trio, duo, da forma que quiser, com quem quiser.</p>
-            </div>
-            <div className="portfolio_item-links">
-              {/* <a href="https://github.com/CarloszDev" className="btn" target='_blank'>Github</a> */}
-              <a href="" className='btn btn-primary' target='_blank'>Ver</a>
-            </div>
+          <div className="portfolio_item-links">
+            <a 
+                href="https://play.google.com/store/apps/details?id=br.com.deway.cyapp&pcampaignid=web_share" 
+                className='btn' 
+                target='_blank'
+                onClick={(e) => e.stopPropagation()}
+              >
+                Ver
+            </a>
+          </div>
+        </article>
+
+        <article 
+          className={`portifolio_item ${activeCard === 3 ? 'active' : ''}`} 
+          onClick={() => handleCardClick(3)}
+        >
+          <div className="portfolio_item-image">
+            <img src={Img3} alt="Transportadora Economica" />
+          </div>
+          <h3>TRANSPORTADORA ECONOMICA</h3>
+          <div className="portfolio_description">
+            <p>Projeto web, oferece uma plataforma onde os usuários podem fazer login e rastrear suas cargas em tempo real. A interface é intuitiva e permite consultar o status e os detalhes das entregas de forma simples e eficiente.</p>
+          </div>
+          <div className="portfolio_item-links">
+            <a 
+                href="http://www.transeconomica.com.br" 
+                className='btn active' 
+                target='_blank'
+                onClick={(e) => e.stopPropagation()}
+              >
+                Ver
+            </a>
+          </div>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Projects;
